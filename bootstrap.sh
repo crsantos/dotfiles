ac9b3ee62a4b737a19df12c39178bebf159d50e7
Carlos Santos <carlosricardosantos@gmail.com>
2019-05-26 12:55:09 +0100

    Bootstrap.sh only syncs fish configs for now
    TODO: apply this to all the ~/ configs

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,22 +1,22 @@
 #!/usr/bin/env bash
 
-cd "$(dirname "${BASH_SOURCE}")";
+# git pull origin master;
 
-git pull origin master;
-
-function doIt() {
+function syncFishFolder() {
 	rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" \
-		--exclude "README.md" --exclude "LICENSE-MIT.txt" -avh --no-perms . ~;
-	source ~/.bash_profile;
+		--exclude "README.md" --exclude "LICENSE-MIT.txt" -avh --no-perms ./fish/ ~/.config/fish;
+
+	# Reload fish shell after that
+	exec /usr/local/bin/fish -l;
 }
 
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
-	doIt;
+	syncFishFolder;
 else
 	read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
 	echo "";
 	if [[ $REPLY =~ ^[Yy]$ ]]; then
-		doIt;
+		syncFishFolder;
 	fi;
 fi;
-unset doIt;
+unset syncFishFolder;
