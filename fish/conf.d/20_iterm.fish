6b48a8745eb867b96e0cd3e588e65834ad49c6d8
Carlos Santos <carlosricardosantos@gmail.com>
2020-10-05 11:10:54 +0100

    Cleaning up and adding starfish

---
+++ fish/conf.d/20_iterm.fish
@@ -0,0 +1,3 @@
+if test $TERM_PROGRAM = iTerm.app
+    test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish
+end
--- fish/config.fish
+++ fish/config.fish
@@ -11,25 +11,20 @@
 #     end
 # end
 
-# Load asdf
-#source /usr/local/Cellar/asdf/*/asdf.fish
-source ~/.asdf/asdf.fish
-
 # Load asdf-java
 sh ~/.asdf/plugins/java/asdf-java-wrapper.bash
 
 # Load SPM completions
 #sh ~/.swift-package-complete.bash
 
+# Load starfish
+starship init fish | source
+
 # old rbenv config
 #status --is-interactive; and source (rbenv init -|psub)
 
 # Load ENV vars from file
 export (cat ~/.env | xargs -L 1)
-
-if test $TERM_PROGRAM = iTerm.app
-    test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish
-end
 
 set JAVA_HOME ~/.asdf/installs/java/openjdk-11.0.1/Contents/Home/bin
 
@@ -45,25 +40,7 @@ set -U fish_key_bindings fish_default_key_bindings #fish_vi_key_bindings
 
 if not set -q abbrs_initialized
   set -U abbrs_initialized
-  echo -n Setting abbreviations... 
+  echo -n "Setting abbreviations..."
 
-  abbr x 'xed .'
-  abbr g 'git'
-  abbr ga 'git add'
-  abbr gb 'git branch'
-  abbr gbl 'git blame'
-  abbr gc 'git commit -m'
-  abbr gco 'git checkout'
-  abbr gcp 'git cherry-pick'
-  abbr gd 'git diff'
-  abbr gf 'git fetch'
-  abbr gl 'git log'
-  abbr gm 'git merge'
-  abbr gp 'git push'
-  abbr gpl 'git pull'
-  abbr gr 'git remote'
-  abbr gs 'git status'
-  abbr gst 'git stash'
-
-  echo 'Done'
+  source ~/.config/fish/abbreviations/all.fish
 end
